6bf72a6bdc69f2a2a58ad1cd08d633c24dc3b483
Maksym Khimii <meizovicmaks@gmail.com>
2023-02-22 14:06:47 +0200

    MorseCodeDecoder task solution (6 kyu)

---
+++ src/test/java/org/example/MorseCodeDecoderTest.java
@@ -0,0 +1,11 @@
+package org.example;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MorseCodeDecoderTest {
+    @Test
+    public void testExampleFromDescription() {
+        assertThat(MorseCodeDecoder.decode("...---..."), is("SOS"));
+    }
+}
